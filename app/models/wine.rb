99bc011f47b3ae028c17ba3c494f6a4657941266
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-08-28 12:27:49 +0200

    add model validation

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,6 @@
 class Wine < ApplicationRecord
+    validates :lowest_price, presence: true
+    validates :highest_price, presence: true
+    validates :average_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+    
 end
--- test/models/wine_test.rb
+++ test/models/wine_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class WineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid attributes" do
+    wine = Wine.new(
+      lowest_price: 10.00,
+      highest_price: 50.00,
+      average_rating: 7.5
+    )
+    assert wine.valid?
+  end
+
+  test "should not be valid without lowest_price" do
+    wine = Wine.new(lowest_price: nil)
+    assert_not wine.valid?
+  end
+
+  test "should not be valid without highest_price" do
+    wine = Wine.new(highest_price: nil)
+    assert_not wine.valid?
+  end
+
+  test "should not be valid without rating" do
+    wine = Wine.new(average_rating: nil)
+    assert_not wine.valid?
+  end
+
+  test "should not be valid if rating is less than 0" do
+    wine = Wine.new(average_rating: -1)
+    assert_not wine.valid?
+  end
+
+  test "should not be valid if rating is greater than 10" do
+    wine = Wine.new(average_rating: 11)
+    assert_not wine.valid?
+  end
 end
